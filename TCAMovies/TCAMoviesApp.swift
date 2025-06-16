f2b0db7b213538147b3ba47e2527073d3e603355
Tomasz Ogrodowski <tomasz.ogrodowski@itcraft.pl>
2025-06-16 13:01:28 +0200

    Initial Commit

---
+++ TCAMovies/TCAMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAMoviesApp.swift
+//  TCAMovies
+//
+//  Created by itcraft on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TCAMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
