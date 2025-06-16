e78e27586ea45df6b7a5f736d0b1a9c83c4a24d1
Tomasz Ogrodowski <tomasz.ogrodowski@itcraft.pl>
2025-06-16 13:03:52 +0200

    commit from sourcetree

--- TCAMovies/ContentView.swift
+++ TCAMovies/ContentView.swift
@@ -3,10 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Text here")
         }
         .padding()
     }
