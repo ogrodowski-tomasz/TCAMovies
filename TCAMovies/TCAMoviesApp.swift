b0fe40e1401dea54b8c7e55bba27cdcd273650d5
Tomasz Ogrodowski <tomasz.ogrodowski@itcraft.pl>
2025-09-23 12:19:38 +0200

    Initial Commit

---
+++ TCAMovies/TCAMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAMoviesApp.swift
+//  TCAMovies
+//
+//  Created by itcraft on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TCAMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCAMoviesTests/TCAMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCAMoviesTests.swift
+//  TCAMoviesTests
+//
+//  Created by itcraft on 23/09/2025.
+//
+
+import Testing
+@testable import TCAMovies
+
+struct TCAMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
